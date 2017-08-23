3346647e6115b580b54ecc5e730969d5767333ec
Suresh Kumar Devaraj <dpsures@gmail.com>
2017-08-23 22:44:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import "hammerjs";
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,MaterialModule,BrowserAnimationsModule,FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
